4928dd154ba3d2ad5307fd7b3e29ecfb8b0b319f
mjzulqarnain <zulqarnainnazir4@gmail.com>
2016-09-06 10:48:38 -0700

    api change for test

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
  
 
+ var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+
+server.listen(3000);
+
+
+ /*
+
 var express = require('express'),
   config = require('./config/config'),
   glob = require('glob'),
@@ -23,3 +33,25 @@ app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
 
+*/
+
+
+
+app.get('/', function (req,res)
+{
+	res.sendFile(__dirname+'/app/views/send.html');
+});
+
+app.post('/', function(req,res)
+	{
+	//	var data = req.body.data;
+//io.sockets.on('connection', function(socket)/
+//	{
+//		socket.on('send', function(data)
+//		{
+			console.log("data");
+			io.sockets.emit('mess',"data");
+//		});
+//	});
+		
+	});
